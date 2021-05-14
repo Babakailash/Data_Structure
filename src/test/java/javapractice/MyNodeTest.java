4efcbdb530a54e366aa4a2dfd774644e7384b249
Babakailash <nathkailash2020@gmail.com>
2021-05-14 20:46:49 +0530

    Added:create linkedlist first test case

--- src/test/java/javapractice/MyNodeTest.java
+++ src/test/java/javapractice/MyNodeTest.java
@@ -1,11 +1,19 @@
 package javapractice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MyNodeTest {
     @Test
     public void given3NumbersWhenLinkedShouldPassedListTest(){
-
+         MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+         MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+         MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+         myFirstNode.setNext(mySecondNode);
+         mySecondNode.setNext(myThirdNode);
+         boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                          mySecondNode.getNext().equals(myThirdNode);
+        Assertions.assertTrue(result);
     }
 
 }
