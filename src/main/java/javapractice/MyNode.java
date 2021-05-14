82dfd28524b4b79f6498b4b39d8c975577209b93
Babakailash <nathkailash2020@gmail.com>
2021-05-14 14:39:08 +0530

    Added:DataStructure file push on git for demo

---
+++ src/main/java/javapractice/MyNode.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class MyNode<T> {
+    private K key;
+    private MyNode next;
+
+    public MyNode(K key) {
+    this.key=key;
+    this.next=null;
+    }
+}
---
+++ src/test/java/javapractice/MyNodeTest.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+import org.junit.jupiter.api.Test;
+
+public class MyNodeTest {
+    @Test
+    public void given3NumbersWhenLinkedShouldPassedListTest(){
+
+    }
+
+}
