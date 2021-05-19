ef3710eaf856ecc2fa57ea15cf608906fbdf9b19
Babakailash <nathkailash2020@gmail.com>
2021-05-19 20:22:41 +0530

    Added:3 node in BST 56->30->70

--- src/test/java/javapractice/BSTTest.java
+++ src/test/java/javapractice/BSTTest.java
@@ -14,25 +14,4 @@ public class BSTTest {
         myBinaryTree.printMyBinaryNode();
         Assertions.assertEquals(3, size);
     }
-
-    @Test
-    public void given13NumbersWhenAddedToABinarySearchTreeShouldReturn13() {
-        BST<Integer> myBinaryTree = new BST<>();
-        myBinaryTree.add(56);
-        myBinaryTree.add(30);
-        myBinaryTree.add(70);
-        myBinaryTree.add(22);
-        myBinaryTree.add(40);
-        myBinaryTree.add(60);
-        myBinaryTree.add(95);
-        myBinaryTree.add(11);
-        myBinaryTree.add(65);
-        myBinaryTree.add(3);
-        myBinaryTree.add(16);
-        myBinaryTree.add(63);
-        myBinaryTree.add(67);
-        Integer size = myBinaryTree.getSize();
-        myBinaryTree.printMyBinaryNode();
-        Assertions.assertEquals(13, size);
-    }
 }
